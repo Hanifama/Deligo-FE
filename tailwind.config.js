85d4358143babb9244530bcce274e4c2362e39ef
hanifama <hanifamafisial.com>
2025-01-05 14:36:28 +0700

    fix/Deligo/fix configurasi build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 export default {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-    "./public/index.html"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
